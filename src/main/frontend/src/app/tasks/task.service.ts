753efb542edb2113ea58e53b69dbc05dd52af2ed
Nikita <nikitakholdobin@yandex.ru>
2019-07-17 16:44:47 +0300

    Change components appearance

---
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from '@angular/common/http'
+
+@Injectable()
+export class TaskService {
+    constructor(private http: HttpClient){
+
+    }
+
+    getTasks() {
+        return this.http.get("/api/tasks");
+    }
+}
